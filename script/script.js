12c48a85b064eb160ac772ee333972d916741343
Санжар <sanjar@san.com>
2020-12-24 23:17:26 +0300

    HomeWork JS

---
+++ script/script.js
@@ -0,0 +1,25 @@
+var myName = prompt('Введите своё имя')
+alert('Привет ' +  myName + ' !')
+console.info('Меня зовут ' + myName)
+
+var age = +prompt('Сколько вам лет?')
+console.info('Мне ' + age)
+
+var example1 = prompt('Решите задачу 7 + 3 = ?' )
+console.info('Пример 1: 7 + 3 = ' + (7 + 3) + ' Ваш ответ ' + example1)
+
+var example2 = prompt('Решите задачу 15 - 5 = ?')
+console.info('Пример 2: 15 - 5 = ' + (15 - 5) + ' Ваш ответ ' + example2)
+
+var example3 = prompt('Решите задачу 5 * 3 = ?')
+console.info('Пример 3: 5 * 3 = ' + 5 * 3 + ' Ваш ответ ' + example3)
+
+var example4 = prompt('Решите задачу 18 / 2 = ?')
+console.info('Пример 4: 18 / 4 = ' + 18 / 2 + ' Ваш ответ ' + example4)
+
+var example5 = prompt('Решите задачу 10 % 3 = ?')
+console.info('Пример 5: 10 % 3 = ' + 10 % 3 + ' Ваш ответ ' + example5)
+
+alert('Откройте консоль')
+
+
